ae5c5cbf29e021f816002dda0e4c0cc806775f93
Tuffy <yusheng831143@gmail.com>
2022-04-06 10:44:40 +0800

    Upload by PicGo

---
+++ mark/config.fish
@@ -0,0 +1,6 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /cygdrive/d/Dev/Miniconda3/Scripts/conda.exe "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
